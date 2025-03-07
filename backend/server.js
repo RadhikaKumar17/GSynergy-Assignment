3428b37bf4ff2946cc7d57ee57c665c0305eb4de
RadhikaKumar17 <radhika70428@gmail.com>
2025-03-07 17:42:08 +0530

    port check

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const xlsx = require("xlsx");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+// const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
@@ -41,6 +42,10 @@ app.get("/chart", (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
